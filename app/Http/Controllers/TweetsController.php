82fa9102d2daf4fd5cdb920ee91fff15465cc9bd
t-kusakabe <ttmac0704@gmail.com>
2016-08-09 00:30:17 +0900

    add_tweetsController_and_indexAction
    tweetsコントローラとindexアクションを追加

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -5,14 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Tweet;
 
 class TweetsController extends Controller
 {
     public function index()
     {
-        // $wafu = DB::select('select * from tweets');
-        // bar_dump($wafu);
-        $hoge = array(1, 2, 3, 4, 5);
-        return view('tweets.index')->with('hoge', $hoge);
+        $tweets = Tweet::all();
+        return view('tweets.index')->with('tweets', $tweets);
     }
 }
