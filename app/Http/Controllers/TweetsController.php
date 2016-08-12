d206c348a9a6e7eadc0f9f8aaa72fc2fb8a7cc3d
t-kusakabe <ttmac0704@gmail.com>
2016-08-13 01:47:36 +0900

    add_association_user_and_tweet

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Tweet;
+use Auth;
+
 
 class TweetsController extends Controller
 {
--- app/Tweet.php
+++ app/Tweet.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tweet extends Model
 {
     protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
